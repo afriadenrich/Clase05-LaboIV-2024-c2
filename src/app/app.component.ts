cd11c93f6d63f467018a61d92f9f799da9ca6fe0
AgustinFrich <agustinfrich@gmail.com>
2024-09-26 09:59:38 -0300

    Clase 05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'formularios-2024-2c';
+  public formGroup: FormGroup;
+
+  fb = inject(FormBuilder);
+
+  constructor() {
+    this.formGroup = this.fb.group({
+      usuario: ['', [Validators.required, Validators.maxLength(10)]],
+      clave: [
+        '',
+        [Validators.required, Validators.minLength(6), this.spacesValidator],
+      ],
+    });
+
+    /*
+    this.formGroup = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+    */
+  }
+
+  login() {
+    console.log(this.formGroup);
+    if (this.formGroup.invalid) {
+      console.log('INVALIDO!!');
+    } else {
+      console.log('VALIDO!!!');
+    }
+  }
+
+  spacesValidator(control: AbstractControl): null | object {
+    const valor: string = <string>control.value;
+    const tieneEspacios = valor.includes(' ');
+
+    if (tieneEspacios) {
+      return {
+        espacios: {
+          posicion: valor.indexOf(' '),
+        },
+      };
+    }
+    return null;
+  }
 }
